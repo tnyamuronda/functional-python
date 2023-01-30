383ca50e6b3d05c85662f604d8fed006fe3ea41c
Tendai Nyamuronda <tnyamuronda26@cmc.edu>
2023-01-29 22:55:32 -0800

    Fixed errors

--- Fixme.py
+++ Fixme.py
@@ -5,6 +5,7 @@ Your solutions should use the map and filter functions,
 and not for loops or list comprehensions.
 '''
 
+
 def evens(n):
     '''
     Returns a list of even numbers from 0 to n inclusive.
@@ -20,11 +21,13 @@ def evens(n):
     >>> evens(-1)
     []
     '''
+    result = filter(lambda x: x % 2 == 0, range(n + 1))
+    return list(result)
 
 
 def threes(n):
     '''
-    Returns a list of all numbers from 0 to n inclusive that contain the digit 3.
+    Returns a list of all numbers from 0 to n inclusive that contain a 3.
 
     >>> threes(2)
     []
@@ -37,11 +40,13 @@ def threes(n):
     >>> threes(50)
     [3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43]
     '''
+    result = filter(lambda x: "3" in str(x), range(n + 1))
+    return list(result)
 
 
 def small_words(text):
     '''
-    Returns a list of all words in the input text that are less than 5 characters long.
+    Returns a list of all words in the input text that are < 5 characters long.
 
     HINT:
     Recall that text.split() converts the text variable into a list of words.
@@ -55,6 +60,9 @@ def small_words(text):
     >>> small_words('a big word is bad')
     ['a', 'big', 'word', 'is', 'bad']
     '''
+    words = text.split()
+    result = filter(lambda x: len(x) < 5, words)
+    return list(result)
 
 
 def squares(n):
@@ -71,11 +79,13 @@ def squares(n):
     >>> squares(10)
     [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
     '''
+    result = map(lambda x: x**2, range(1, n + 1))
+    return list(result)
 
 
 def lengths(strings):
     '''
-    Given a list of strings, returns a list of the lengths of the corresponding strings.
+    Given list of strings, returns list of lengths of the corresponding strings
 
     >>> lengths([])
     []
@@ -84,3 +94,5 @@ def lengths(strings):
     >>> lengths(['this','is','a','test'])
     [4, 2, 1, 4]
     '''
+    result = map(lambda x: len(x), strings)
+    return list(result)
